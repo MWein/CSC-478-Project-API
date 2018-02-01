20b9a5d9e7661cefcd23243bfe37559393906dcf
Mike <mweinberg21@gmail.com>
2018-02-01 11:22:44 -0600

    Reformated a thing

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const port = 8081
 app.listen(port, () => {
   console.log(' --------------------------------------- ') // eslint-disable-line no-console
   console.log('|                                       |') // eslint-disable-line no-console
-  console.log(`|  API listening on port ${port}           |`) // eslint-disable-line no-console
+  console.log(`|     API listening on port ${port}        |`) // eslint-disable-line no-console
   console.log('|                                       |') // eslint-disable-line no-console
   console.log(' --------------------------------------- ') // eslint-disable-line no-console
 })
