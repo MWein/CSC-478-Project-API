f2f2b0531c890b89379fdafe3ca1b7052e315d10
Mike <mweinberg21@gmail.com>
2018-02-04 19:25:44 -0600

    Added warning to start up console log

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.listen(port, () => {
   console.log(' --------------------------------------- ') // eslint-disable-line no-console
   console.log('|                                       |') // eslint-disable-line no-console
   console.log(`|     API listening on port ${port}        |`) // eslint-disable-line no-console
+  console.log('|      DO NOT CLOSE THIS WINDOW         |') // eslint-disable-line no-console
   console.log('|                                       |') // eslint-disable-line no-console
   console.log(' --------------------------------------- ') // eslint-disable-line no-console
 })
