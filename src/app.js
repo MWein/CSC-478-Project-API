1ddede97cde06c37bd9b4933c89f47c673572752
Mike <mweinberg21@gmail.com>
2018-02-12 23:39:37 -0600

    Added bodyParser middleware to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 import routes from './routes'
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', routes)
 
