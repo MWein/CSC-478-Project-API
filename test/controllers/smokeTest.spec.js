ffc62ca134eea1229c30efb673fd5f2ca51badf7
Mike <mweinberg21@gmail.com>
2018-02-13 16:56:49 -0600

    Rewrote smoketest.spec to use chai and sinon-express-mock

--- test/controllers/smokeTest.spec.js
+++ test/controllers/smokeTest.spec.js
@@ -1,13 +1,22 @@
-import app from '../../src/app'
-import request from 'supertest'
+import smokeTestController from '../../src/controllers/smokeTest'
+import chai from 'chai'
+import { mockReq, mockRes } from 'sinon-express-mock'
+import sinon from 'sinon'
+
+chai.use(require('sinon-chai'))
+const expect = chai.expect
 
 
 describe('Smoketest... test', () => {
-  it('Responds with 200 and "ok"', done => {
-    request(app).get('/status').then(response => {
-      expect(response.text).toEqual('ok')
-      expect(response.statusCode).toEqual(200)
-      done()
-    })
+  it('Returns 200 and ok', () => {
+    const req = mockReq()
+    const res = mockRes()
+    const next = sinon.spy()
+  
+    smokeTestController(req, res, next)
+  
+    expect(res.status).to.be.calledWith(200)
+    expect(res.send).to.be.calledWith('ok')
+    expect(next.calledOnce).to.equal(true)
   })
 })
