4d307dd9e4c73282ed72132a8add8329285069f6
Mike <mweinberg21@gmail.com>
2018-02-12 23:39:52 -0600

    Added queries for users table in database

--- src/db/queries.js
+++ src/db/queries.js
@@ -2,7 +2,23 @@ export const selectAllRows = () => ({
   text: 'SELECT * FROM "test_table"',
 })
 
-// module.exports.selectEvent = id => ({
-//   text: 'SELECT * FROM "table_name" WHERE event_id = ($1)',
-//   values: [ id ],
-// })
+
+// users table queries
+export const allUsers = () => ({
+  text: 'SELECT * FROM "users"',
+})
+
+export const getUserRow = id => ({
+  text: 'SELECT * FROM "users" WHERE id = ($1)',
+  values: [ id ],
+})
+
+export const updateTimestampForUser = (id, timestamp) => ({
+  text: 'UPDATE users SET timestamp = ($1) WHERE id = ($2)',
+  values: [ timestamp, id ],
+})
+
+export const updateTokenAndTimestampForUser = (id, token, timestamp) => ({
+  text: 'UPDATE users SET token = ($1), timestamp = ($2) WHERE id = ($3)',
+  values: [ token, timestamp, id ],
+})
