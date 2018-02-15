55e62d32c696bc653ca2098e03d43e81ac1efbc2
Mike <mweinberg21@gmail.com>
2018-02-14 21:44:45 -0600

    Removed comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,11 +12,9 @@ const router = express.Router() // eslint-disable-line new-cap
 
 router.get('/status', smokeTest)
 
-// Logging in and out
 router.post('/login', loginController)
 router.post('/logout', logoutController)
 
-// User management related routes
 router.post('/allUsers', getUser, permit('admin'), getAllUsersController)
 router.post('/createUser', getUser, permit('admin'), createUserController)
 // router.post('/editUser', getUser, permit('admin'), editUserController)
