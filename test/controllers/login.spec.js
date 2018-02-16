a58bd501d1ea0746de5b4e9f44654ac17a4ecd91
Minuteman92 <mweinberg21@gmail.com>
2018-02-15 23:10:56 -0600

    Moved generateUniqueKey tests to own file

--- test/controllers/login.spec.js
+++ test/controllers/login.spec.js
@@ -11,35 +11,6 @@ const expect = chai.expect
 const generateUniqueKey = genUniqKey.generateUniqueKey
 
 
-describe('generateUniqueKey tests', () => {
-  it('generateUniqueKey generates a unique key', () => {
-    const mockKeys = [
-      'ghKjfdUkfj',
-      'oPfjdkslaj',
-      'hYkkdhsLui',
-    ]
-    const actual = generateUniqueKey(mockKeys)
-
-    expect(mockKeys.includes(actual)).to.equal(false)
-  })
-
-  it('generateUniqueKey does not create the same key in consequtive calls', () => {
-    const mockKeys = [
-      'ghKjfdUkfj',
-      'oPfjdkslaj',
-      'hYkkdhsLui',
-    ]
-    const first = generateUniqueKey(mockKeys)
-    const second = generateUniqueKey([...mockKeys, first])
-    const third = generateUniqueKey([...mockKeys, first, second])
-
-    expect(first).not.to.equal(second)
-    expect(first).not.to.equal(third)
-    expect(second).not.to.equal(third)
-  })
-})
-
-
 describe('Login controller tests', () => {
 
   let dbStub
---
+++ test/helpers/generateUniqueKey.spec.js
@@ -0,0 +1,40 @@
+import loginController from '../../src/controllers/login'
+import { mockReq, mockRes } from 'sinon-express-mock'
+import chai from 'chai'
+import db from '../../src/db/index'
+import sinon from 'sinon'
+import genUniqKey from '../../src/helpers/generateUniqueKey'
+
+chai.use(require('sinon-chai'))
+const expect = chai.expect
+
+const generateUniqueKey = genUniqKey.generateUniqueKey
+
+
+describe('generateUniqueKey tests', () => {
+  it('generateUniqueKey generates a unique key', () => {
+    const mockKeys = [
+      'ghKjfdUkfj',
+      'oPfjdkslaj',
+      'hYkkdhsLui',
+    ]
+    const actual = generateUniqueKey(mockKeys)
+
+    expect(mockKeys.includes(actual)).to.equal(false)
+  })
+
+  it('generateUniqueKey does not create the same key in consequtive calls', () => {
+    const mockKeys = [
+      'ghKjfdUkfj',
+      'oPfjdkslaj',
+      'hYkkdhsLui',
+    ]
+    const first = generateUniqueKey(mockKeys)
+    const second = generateUniqueKey([...mockKeys, first])
+    const third = generateUniqueKey([...mockKeys, first, second])
+
+    expect(first).not.to.equal(second)
+    expect(first).not.to.equal(third)
+    expect(second).not.to.equal(third)
+  })
+})
