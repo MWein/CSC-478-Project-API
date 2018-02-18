0dc43a1fcdfac99d7522e2d200ce83a0d03158fb
Mike <mweinberg21@gmail.com>
2018-02-18 16:43:44 -0600

    Added customerByPhone query

--- src/db/customerManagement.js
+++ src/db/customerManagement.js
@@ -7,6 +7,11 @@ export const customerById = id => ({
   values: [ id ],
 })
 
+export const customerByPhone = phone => ({
+  text: 'SELECT * FROM "customers" WHERE phone = ($1)',
+  values: [ phone ],
+})
+
 export const editCustomer = (id, f_name, l_name, phone, address, active, email) => ({
   text: 'UPDATE customers SET f_name = ($2), l_name = ($3), phone = ($4), address = ($5), active = ($6), email = ($7) WHERE id = ($1)',
   values: [ id, f_name, l_name, phone, address, active, email ],
