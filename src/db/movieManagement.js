f7906263f461516d4edebe0c69e56ba0bc019f5f
Mike <mweinberg21@gmail.com>
2018-02-22 01:17:34 -0600

    Movie management queries

---
+++ src/db/movieManagement.js
@@ -0,0 +1,22 @@
+export const allMovies = () => ({
+  text: 'SELECT * FROM "movies"',
+})
+
+export const allUPCs = () => ({
+  text: 'SELECT upc FROM "movies"',
+})
+
+export const getMovieRow = upc => ({
+  text: 'SELECT * FROM "movies" WHERE upc = ($1)',
+  values: [ upc ],
+})
+
+export const createMovie = (upc, title, poster, copies) => ({
+  text: 'INSERT INTO movies (upc, title, poster, copies) VALUES (($1), ($2), ($3), ($4))',
+  values: [ upc, title, poster, copies ],
+})
+
+export const editMovie = (upc, title, poster, copies) => ({
+  text: 'UPDATE users SET title = ($2), poster_loc = ($3), copies = ($4) WHERE upc = ($1)',
+  values: [ upc, title, poster, copies ],
+})
