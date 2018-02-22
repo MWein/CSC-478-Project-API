449c73e35099a0e665bdb7a3cdec44dd17284fa5
Mike <mweinberg21@gmail.com>
2018-02-22 16:43:52 -0600

    Started writing more healthv2 test... work happened

--- test/controllers/healthvs.spec.js
+++ test/controllers/healthvs.spec.js
@@ -15,6 +15,11 @@ describe('Health V2 test', () => {
     dbStub.restore()
   })
 
+  const noErrorsReturn = {
+    error: false,
+    errorMsg: '',
+  }
+
 
   it('Successfully returns health of API if all tables exist', async() => {
     dbStub = sinon.stub(db, 'sqlQuery').returns({ error: false, errorMsg: '' })
@@ -37,6 +42,34 @@ describe('Health V2 test', () => {
   })
 
 
+  it('Successfully creates all tables if they dont already exist', async() => {
+    dbStub = sinon.stub(db, 'sqlQuery')
+      .onCall(0).returns({ error: true, errorMsg: 'relation "users" does not exist' })
+      .onCall(1).returns(noErrorsReturn) // Create users table
+      .onCall(3).returns(noErrorsReturn) // Create superuser
+      .onCall(4).returns({ error: true, errorMsg: 'relation "customers" does not exist' })
+      .onCall(5).returns(noErrorsReturn) // Create customers table
+      .onCall(6).returns({ error: true, errorMsg: 'relation "movies" does not exist' })
+      .onCall(7).returns(noErrorsReturn) // Create movies table
+
+    const req = mockReq()
+    const res = mockRes()
+    const next = sinon.spy()
+
+    await healthController(req, res, next)
+
+    expect(dbStub.callCount).to.equal(8)
+    expect(res.status).to.be.calledWith(200)
+    expect(res.json).to.be.calledWith({
+      customersDatabase: { error: false, errorMsg: '' },
+      moviesDatabase: { error: false, errorMsg: '' },
+      usersDatabase: { error: false, errorMsg: '' },
+      error: false,
+    })
+    expect(next).to.be.called
+  })
+
+
   // it('Successfully creates user table if it does not exist', () => {  
   //   const usersTableError = {
   //     error: true,
@@ -48,8 +81,5 @@ describe('Health V2 test', () => {
   //     .onCall(1).returns({ error: false, errorMsg: '' })
 
 
-    
   // })
-
-
 })
