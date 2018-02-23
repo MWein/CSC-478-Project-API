5f6d35240e0e30ac827e58855e9c5c2172c0084b
Mike <mweinberg21@gmail.com>
2018-02-22 19:56:39 -0600

    Updated /status with new function that checks connections

--- src/controllers/health.js
+++ src/controllers/health.js
@@ -1,5 +1,83 @@
-const healthController = (req, res, next) => {
-  res.status(200).send('ok')
+import {
+  checkCustomersTable,
+  checkMoviesTable,
+  checkUsersTable,
+  createCustomersTable,
+  createMoviesTable,
+  createUsersTable,
+} from '../db/createTables'
+import { createUser } from '../db/userManagement'
+import { sqlQuery } from '../db'
+
+
+const tableCheckReturn = async(check, tableName, createTableQuery) => {
+  if (check.error && check.errorMsg.includes('does not exist')) {
+    const usersTableCreation = await sqlQuery(createTableQuery)
+
+    if (usersTableCreation.error) {
+      return { error: true, errorMsg: `Could not create ${tableName} table` }
+    }
+  } else if (check.error) {
+    return { error: true, errorMsg: `Could not reach ${tableName} table` }
+  }
+
+  return { error: false, errorMsg: '' }
+}
+
+
+export const checkForUsersTable = async() => {
+  const usersCheck = await sqlQuery(checkUsersTable())
+  const returnVal = await tableCheckReturn(usersCheck, 'users', createUsersTable())
+
+  return returnVal
+}
+
+
+export const checkForCustomersTable = async() => {
+  const customersCheck = await sqlQuery(checkCustomersTable())
+  const returnVal = await tableCheckReturn(customersCheck, 'customers', createCustomersTable())
+
+  return returnVal
+}
+
+
+export const checkForMoviesTable = async() => {
+  const moviesCheck = await sqlQuery(checkMoviesTable())
+  const returnVal = await tableCheckReturn(moviesCheck, 'movies', createMoviesTable())
+
+  return returnVal
+}
+
+
+const healthController = async(req, res, next) => {
+  const usersStatus = await checkForUsersTable()
+  const customersStatus = await checkForCustomersTable()
+  const moviesStatus = await checkForMoviesTable()
+
+
+  await sqlQuery(createUser('superuser', '', '', 'password', 'admin', true))
+
+
+  // TODO Check connection to IMDB
+
+
+  const returnVal = {
+    usersDatabase: usersStatus,
+    customersDatabase: customersStatus,
+    moviesDatabase: moviesStatus,
+  }
+
+
+  const checkStatus = Object.values(returnVal).reduce((acc, object) => object.error ? acc + 1 : acc, 0)
+  const status = checkStatus === 0 ? 200 : 500
+  const error = checkStatus !== 0
+
+  const finalReturnVal = {
+    ...returnVal,
+    error,
+  }
+
+  res.status(status).json(finalReturnVal)
   next()
 }
 
--- test/controllers/health.spec.js
+++ test/controllers/health.spec.js
@@ -1,22 +1,201 @@
+import healthController,
+{
+  checkForCustomersTable,
+  checkForMoviesTable,
+  checkForUsersTable,
+} from '../../src/controllers/health'
 import { mockReq, mockRes } from 'sinon-express-mock'
 import chai from 'chai'
-import healthController from '../../src/controllers/health'
+import db from '../../src/db'
 import sinon from 'sinon'
 
 chai.use(require('sinon-chai'))
 const expect = chai.expect
 
 
-describe('Smoketest... test', () => {
-  it('Returns 200 and ok', () => {
+describe('Health tests', () => {
+  let dbStub
+
+  afterEach(() => {
+    dbStub.restore()
+  })
+
+  const noErrorsReturn = {
+    error: false,
+    errorMsg: '',
+  }
+
+
+  it('Successfully returns health of API if all tables exist', async() => {
+    dbStub = sinon.stub(db, 'sqlQuery').returns({ error: false, errorMsg: '' })
+
     const req = mockReq()
     const res = mockRes()
     const next = sinon.spy()
 
-    healthController(req, res, next)
+    await healthController(req, res, next)
 
+    expect(dbStub.callCount).to.equal(4)
     expect(res.status).to.be.calledWith(200)
-    expect(res.send).to.be.calledWith('ok')
-    expect(next.calledOnce).to.equal(true)
+    expect(res.json).to.be.calledWith({
+      customersDatabase: noErrorsReturn,
+      moviesDatabase: noErrorsReturn,
+      usersDatabase: noErrorsReturn,
+      error: false,
+    })
+    expect(next).to.be.called
+  })
+
+
+  it('Returns errors if database is not reachable', async() => {
+    dbStub = sinon.stub(db, 'sqlQuery').returns({ error: true, errorMsg: '' })
+
+    const req = mockReq()
+    const res = mockRes()
+    const next = sinon.spy()
+
+    await healthController(req, res, next)
+
+    expect(dbStub.callCount).to.equal(4)
+    expect(res.status).to.be.calledWith(500)
+    expect(res.json).to.be.calledWith({
+      customersDatabase: { error: true, errorMsg: 'Could not reach customers table' },
+      moviesDatabase: { error: true, errorMsg: 'Could not reach movies table' },
+      usersDatabase: { error: true, errorMsg: 'Could not reach users table' },
+      error: true,
+    })
+    expect(next).to.be.called
+  })
+
+
+  describe('checkForUsersTable tests', () => {
+    it('Returns an error if table is unreachable', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery').returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForUsersTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not reach users table' })
+    })
+
+    it('Throws error if table does not exist and creation fails', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'users table does not exist' })
+        .onCall(1).returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForUsersTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not create users table' })
+    })
+
+    it('Returns success if table does not exist but is successfully created', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'users table does not exist' })
+        .onCall(1).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForUsersTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
+
+    it('Returns success if table exists', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForUsersTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
+  })
+
+
+  describe('checkForCustomersTable tests', () => {
+    it('Returns an error if table is unreachable', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery').returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForCustomersTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not reach customers table' })
+    })
+
+    it('Throws error if table does not exist and creation fails', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'users table does not exist' })
+        .onCall(1).returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForCustomersTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not create customers table' })
+    })
+
+    it('Returns success if table does not exist but is successfully created', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'customers table does not exist' })
+        .onCall(1).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForCustomersTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
+
+    it('Returns success if table exists', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForCustomersTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
+  })
+
+
+  describe('checkForMoviesTable tests', () => {
+    it('Returns an error if table is unreachable', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery').returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForMoviesTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not reach movies table' })
+    })
+
+    it('Throws error if table does not exist and creation fails', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'users table does not exist' })
+        .onCall(1).returns({ error: true, errorMsg: '' })
+
+      const actual = await checkForMoviesTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: true, errorMsg: 'Could not create movies table' })
+    })
+
+    it('Returns success if table does not exist but is successfully created', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: true, errorMsg: 'movies table does not exist' })
+        .onCall(1).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForMoviesTable()
+
+      expect(dbStub.callCount).to.equal(2)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
+
+    it('Returns success if table exists', async() => {
+      dbStub = sinon.stub(db, 'sqlQuery')
+        .onCall(0).returns({ error: false, errorMsg: '' })
+
+      const actual = await checkForMoviesTable()
+
+      expect(dbStub.callCount).to.equal(1)
+      expect(actual).to.deep.equal({ error: false, errorMsg: '' })
+    })
   })
 })
