c6a46c7ff0d45802a9d445241a2f8b313255e9a4
Mike <mweinberg21@gmail.com>
2018-02-22 19:29:17 -0600

    Refactored healthv2

--- src/controllers/healthv2.js
+++ src/controllers/healthv2.js
@@ -9,80 +9,66 @@ import {
 import { createUser } from '../db/userManagement'
 import { sqlQuery } from '../db'
 
-const healthController = async(req, res, next) => {
-  const checkForUsersTable = async() => {
-    const usersCheck = await sqlQuery(checkUsersTable())
 
-    if (usersCheck.error && usersCheck.errorMsg === 'relation "users" does not exist') {
-      const usersTableCreation = await sqlQuery(createUsersTable())
+const tableCheckReturn = async(check, tableName, createTableQuery) => {
+  if (check.error && check.errorMsg.contains('does not exist')) {
+    const usersTableCreation = await sqlQuery(createTableQuery)
 
-      if (usersTableCreation.error) {
-        return { error: true, errorMsg: 'Could not create users table' }
-      }
-    } else if (usersCheck.error) {
-      return { error: true, errorMsg: 'Could not reach users table' }
+    if (usersTableCreation.error) {
+      return { error: true, errorMsg: `Could not create ${tableName} table` }
     }
-
-    return { error: false, errorMsg: '' }
+  } else if (check.error) {
+    return { error: true, errorMsg: `Could not reach ${tableName} table` }
   }
-  const usersCheck = await checkForUsersTable()
+
+  return { error: false, errorMsg: '' }
+}
+
+
+export const checkForUsersTable = async() => {
+  const usersCheck = await sqlQuery(checkUsersTable())
+  const returnVal = await tableCheckReturn(usersCheck, 'users', createUsersTable())
+
+  return returnVal
+}
+
+
+const checkForCustomersTable = async() => {
+  const customersCheck = await sqlQuery(checkCustomersTable())
+  const returnVal = await tableCheckReturn(customersCheck, 'customers', createCustomersTable())
+
+  return returnVal
+}
+
+
+const checkForMoviesTable = async() => {
+  const moviesCheck = await sqlQuery(checkMoviesTable())
+  const returnVal = await tableCheckReturn(moviesCheck, 'movies', createMoviesTable())
+
+  return returnVal
+}
+
+
+const healthController = async(req, res, next) => {
+  const usersStatus = await checkForUsersTable()
+  const customersStatus = await checkForCustomersTable()
+  const moviesStatus = await checkForMoviesTable()
+
 
   await sqlQuery(createUser('superuser', '', '', 'password', 'admin', true))
-
-
-  const checkForCustomersTable = async() => {
-    const customersCheck = await sqlQuery(checkCustomersTable())
-
-    if (customersCheck.error && customersCheck.errorMsg === 'relation "customers" does not exist') {
-      const customersTableCreation = await sqlQuery(createCustomersTable())
-
-      if (customersTableCreation.error) {
-        return { error: true, errorMsg: 'Could not create customers table' }
-      }
-    } else if (customersCheck.error) {
-      return { error: true, errorMsg: 'Could not reach customers table' }
-    }
-
-    return { error: false, errorMsg: '' }
-  }
-  const customersCheck = await checkForCustomersTable()
-
-
-  const checkForMoviesTable = async() => {
-    const moviesCheck = await sqlQuery(checkMoviesTable())
-
-    if (moviesCheck.error && moviesCheck.errorMsg === 'relation "movies" does not exist') {
-      const moviesTableCreation = await sqlQuery(createMoviesTable())
-
-      if (moviesTableCreation.error) {
-        return { error: true, errorMsg: 'Could not create movies table' }
-      }
-    } else if (moviesCheck.error) {
-      return { error: true, errorMsg: 'Could not reach movies table' }
-    }
-
-    return { error: false, errorMsg: '' }
-  }
-  const moviesCheck = await checkForMoviesTable()
 
 
   // TODO Check connection to IMDB
 
 
   const returnVal = {
-    usersDatabase: usersCheck,
-    customersDatabase: customersCheck,
-    moviesDatabase: moviesCheck,
+    usersDatabase: usersStatus,
+    customersDatabase: customersStatus,
+    moviesDatabase: moviesStatus,
   }
 
 
-  const checkStatus = Object.values(returnVal).reduce((acc, object) => {
-    if (object.error) {
-      return acc + 1
-    }
-
-    return acc
-  }, 0)
+  const checkStatus = Object.values(returnVal).reduce((acc, object) => object.error ? acc + 1 : acc, 0)
   const status = checkStatus === 0 ? 200 : 500
   const error = checkStatus !== 0
 
