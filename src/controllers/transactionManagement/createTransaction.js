19c8ab1492ec2c1996c13d9afd7e964a854ba4b8
Mike <mweinberg21@gmail.com>
2018-04-09 23:41:21 -0500

    createTransaction supports multiple IDs

--- src/controllers/transactionManagement/createTransaction.js
+++ src/controllers/transactionManagement/createTransaction.js
@@ -1,26 +1,22 @@
 import {
-  databaseErrorMessage,
-  noError,
-} from '../../errorMessages'
-import {
   createTransaction,
 } from '../../db/transactionManagement'
+import {
+  noError,
+} from '../../errorMessages'
 import { sqlQuery } from '../../db'
 
 
 const createTransactionController = async(req, res, next) => {
   const customerID = req.body.customerID
-  const copyID = req.body.copyID
-
+  const copyIDs = req.body.copyIDs
   const dueDate = new Date()
 
   dueDate.setDate(dueDate.getDate() + 1)
 
-  const qResult = await sqlQuery(createTransaction(customerID, copyID, dueDate, false))
-
-  if (qResult.error) {
-    return databaseErrorMessage(res)
-  }
+  copyIDs.map(copyID => {
+    sqlQuery(createTransaction(customerID, copyID, dueDate, false))
+  })
 
   res.status(200).json(noError())
   next()
